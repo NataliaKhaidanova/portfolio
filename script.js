3b060cd4c25267f8f1acfff1b89a031e6dfcb368
Natalia <khaidanova@outlook.com>
2024-06-04 16:31:19 +0200

    Formatted.

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ const tabButtons = tabsList.querySelectorAll("a");
 const tabPanels = tabsContainer.querySelectorAll(".tabs_panels > section");
 
 tabButtons.forEach((tab, index) => {
-    if (index > 0) {
-        tabPanels[index].setAttribute("hidden", "");
-    }
+  if (index > 0) {
+    tabPanels[index].setAttribute("hidden", "");
+  }
 });
 
 tabsContainer.addEventListener("click", (e) => {
-    const clickedTab = e.target.closest("a");
-    if (!clickedTab) return;
-    e.preventDefault();
+  const clickedTab = e.target.closest("a");
+  if (!clickedTab) return;
+  e.preventDefault();
 
-    switchTab(clickedTab);
+  switchTab(clickedTab);
 });
 
 function switchTab(clickedTab) {
-    const activePanelId = clickedTab.getAttribute("href");
-    const activePanel = tabsContainer.querySelector(activePanelId);
-    
-    tabPanels.forEach((panel) => {
-        panel.setAttribute("hidden", true);
-    });
-    activePanel.removeAttribute("hidden");
+  const activePanelId = clickedTab.getAttribute("href");
+  const activePanel = tabsContainer.querySelector(activePanelId);
+
+  tabPanels.forEach((panel) => {
+    panel.setAttribute("hidden", true);
+  });
+  activePanel.removeAttribute("hidden");
 }
